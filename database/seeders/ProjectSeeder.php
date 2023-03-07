069b8dc41f14722321a93af32a5895da2ba52357
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-03-07 19:29:51 +0100

    Create seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $projects = config('projects');
+
+        foreach ($projects as $project) {
+            $new_project = new Project();
+            $new_project->fill($project);
+            $new_project->slug = Str::slug($new_project->title, '-');
+            $new_project->save();
+        }
     }
 }
