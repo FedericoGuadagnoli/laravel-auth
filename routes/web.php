859129ed6fd6bc5e82de42259e44649e68c00db8
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-03-07 17:22:08 +0100

    Create AdminHomeController & GuestHomeController

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Guest\HomeController as GuestHomeController;
+use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,13 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-});
+Route::get('/', [GuestHomeController::class, 'index']);
 
-Route::get('/admin', function () {
-    return view('admin.home');
-})->middleware(['auth'])->name('admin.home');
+Route::get('/admin', [AdminHomeController::class, 'index'])->middleware(['auth'])->name('admin.home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
